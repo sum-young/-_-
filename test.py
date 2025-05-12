89ea0693fbbc862bcfaf90a32748cafaaa9af6ba
kingdain39 <dain39@ewha.ac.kr>
2025-05-12 18:46:52 +0900

    정다인브랜치개설

--- test.py
+++ test.py
@@ -1,19 +1,2 @@
-print("test file")
-print("d안녕하세요")
-
-
-print("안녕하세요 :D")
-#test file
-
-print("안녕하시오라오라오라")
-
-print("2차 테스트하기")
-
-print("수정")
-
-
-print("33차 테스트하기")
-print("수정완료임")
-
-print("testtest")
+print("다인 브랜치 만듦")
 
